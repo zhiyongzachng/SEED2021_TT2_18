cb0bd9aa8b8937c4558c46f90f9b76f387c40a88
jang038 <jang038@e.ntu.edu.sg>
2020-12-11 16:02:07 +0800

    Update views.py

--- heimdall/dbsapp/views.py
+++ heimdall/dbsapp/views.py
@@ -1,45 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests 
+import json
+
 # Create your views here.
 def login(request):
 	if request.method == "POST":
-		amount = int(request.POST.get('amount'))
-		amt = -amount
-		MAIN_URL = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/add"
-		result = requests.post(MAIN_URL, headers={'x-api-key': '1elJCmVDjP45as0PT9fsEmHvP03074O8h05lQUmh'},data={"custID":'18',"amount":amt})
+		MAIN_URL = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/view"
+		result = requests.post(MAIN_URL, headers={'x-api-key': '1elJCmVDjP45as0PT9fsEmHvP03074O8h05lQUmh'},data={"custID":'18'})
 		result_dict = result.json()
-		if (result_dict.statusCode == 409)
-			return HttpResponse('fail')
-		else
-			custID = request.POST.get('custID')
-			amount = int(request.POST.get('amount'))
-			amt = amount
-			MAIN_URL = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/add"
-			result = requests.post(MAIN_URL, headers={'x-api-key': '1elJCmVDjP45as0PT9fsEmHvP03074O8h05lQUmh'},data={"custID":custID,"amount":amt})
-			result_dict = result.json()
-			custID = request.POST.get('custID')
-			payeeID = request.POST.get('payeeID')
-			dateTime = request.POST.get('dateTime')
-			amount = request.POST.get('amount')
-			expensesCat = request.POST.get('expensesCat')
-			eGift = request.POST.get('eGift')
-			message = request.POST.get('message')
-			MAIN_URL = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/transaction/add"
-			result = requests.post(MAIN_URL, headers={'x-api-key': '1elJCmVDjP45as0PT9fsEmHvP03074O8h05lQUmh'},data={"custID":custID,"payeeID":payeeID,"dateTime":dateTime,"amount":amount,"expensesCat":expensesCat,"eGift":eGift,"message":message})
-			result_dict = result.json()
-			if result_dict.statusCode == "200"
-				return HttpResponse('hello mark')
-
-    return render(request, "dbsapp/login.html")
+		return render(request, 'account_info.html', context)
 
 def logout(request):
-    return render(request, 'dbsapp/logout.html') 
+	return render(request, 'dbsapp/logout.html') 
 
 def overview(request):
-    return render(request, 'dbsapp/overview.html', {'title': 'About'})
+	return render(request, 'dbsapp/overview.html', {'title': 'About'})
 
 def transaction(request):
-    return render(request, 'dbsapp/transaction.html', {'title': 'About'})
+	return render(request, 'dbsapp/transaction.html', {'title': 'About'})
 
 
