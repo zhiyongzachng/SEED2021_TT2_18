04df8fcf179964af93b1fcb70644122b2a5d2faa
jadotting <jadotting@gmail.com>
2020-12-11 13:57:25 +0800

    my part
    my part

--- test1234/blog/views.py
+++ test1234/blog/views.py
@@ -2,14 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests 
 
-
+#post = [
+#    { 'author' : 'CoreyMS'}
+#]
 # Create your views here.
 
 def home(request):
     MAIN_URL = "https://u8fpqfk2d4.execute-api.ap-southeast-1.amazonaws.com/techtrek2020/users"
     result = requests.post(MAIN_URL, headers={'x-api-key': '1elJCmVDjP45as0PT9fsEmHvP03074O8h05lQUmh'})
     result_dict = result.json()
-    return render(request, 'transferfunds.html', result_dict)
+    context = {
+        'result_dict' : result_dict[1]['phoneNumber']
+    }
+    #print(result_dict[0])
+   
+    answer = []
+    for row in result_dict:
+        phoneNumber = row['phoneNumber']
+        if (phoneNumber == '(+65) 98271258'):
+            userid = row['custID']
+        #value = result_dict[row]['phoneNumber']
+        #print(value)
+        #answer.append(result_dict[row]['id'])
+            context = {
+                'result_dict' : userid
+            }
+            return render(request, 'transferfunds.html', context)
+         
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
